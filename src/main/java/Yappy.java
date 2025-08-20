85827b3ea69183cbc736fa5a58251b59265f8e53
TastyVeggy <alexius2111@gmail.com>
2025-08-20 19:54:58 +0800

    Rename chatbot from Duke to Yappy

---
+++ src/main/java/Yappy.java
@@ -0,0 +1,11 @@
+public class Yappy {
+    public static void main(String[] args) {
+		String logo = "__   __                      \n"
+		+ "\\ \\ / /_ _ _ __  _ __  _   _ \n"
+		+ " \\ V / _` | '_ \\| '_ \\| | | |\n"
+		+ "  | | (_| | |_) | |_) | |_| |\n"
+		+ "  |_|\\__,_| .__/| .__/ \\__, |\n"
+		+ "	  |_|   |_|    |___/\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
