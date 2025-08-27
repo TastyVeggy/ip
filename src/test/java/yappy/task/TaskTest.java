9930579bc9741be6c7c27d839e46ff9558981b01
TastyVeggy <alexius2111@gmail.com>
2025-08-27 19:23:19 +0800

    Add test for task class

---
+++ src/test/java/yappy/task/TaskTest.java
@@ -0,0 +1,47 @@
+package yappy.task;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import yappy.task.exception.EmptyTaskDescriptionException;
+
+public class TaskTest {
+    private Task task;
+
+
+    @BeforeEach
+    void setUp() throws EmptyTaskDescriptionException {
+        this.task = new Task("Dummy task");
+    }
+
+    @Test
+    void testConstructorCreatesTaskSuccessfully() {
+        assertNotNull(task);
+        assertEquals("[ ] Dummy task", task.toString());
+    }
+
+    @Test
+    void testConstructorThrowsExceptionOnEmptyOrNullDescription() {
+        assertThrows(EmptyTaskDescriptionException.class, () -> {
+            new ToDoTask("");
+        });
+        assertThrows(EmptyTaskDescriptionException.class, () -> {
+            new ToDoTask(null);
+        });
+    }
+
+    @Test
+    void testMarkAndUnmarkTask() {
+        task.markAsDone();
+        assertEquals("X", task.getStatusIcon());
+        task.unmarkAsDone();
+        assertEquals(" ", task.getStatusIcon());
+        task.markAsDone();
+        assertEquals("X", task.getStatusIcon());
+    }
+
+}
