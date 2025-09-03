10c73ee42239c9d03d2f96ca8ab86e15f0b8f428
TastyVeggy <alexius2111@gmail.com>
2025-09-03 21:02:47 +0800

    Refactor Yappy to be compatible with both cli and gui

--- src/main/java/yappy/ui/Yappy.java
+++ src/main/java/yappy/ui/Yappy.java
@@ -1,7 +1,6 @@
 package yappy.ui;
 
 import java.util.Scanner;
-
 import yappy.Constants;
 import yappy.exception.YappyException;
 import yappy.task.TaskList;
@@ -19,80 +18,112 @@ import yappy.util.UiUtil;
  * </ul>
  */
 public class Yappy {
+	private TaskList taskList;
+
+	private record ParsedInput(String commandName, String args) {
+	};
+
+	private record CommandResult(String response, TaskList taskList) {
+	};
+
+	public Yappy() {
+		this.taskList = attemptToLoadTasksFromBackup();
+	}
+
+	public String interact(String input) {
+		ParsedInput parsedInput = parseInput(input);
+		CommandResult result = executeCommand(parsedInput, taskList);
+		this.taskList = result.taskList();
+		return result.response();
+	}
+
+	public static String getGreetings() {
+		return Constants.LOGO + "\n" + "Hello! I'm Yappy\n" + "What can I do for you?";
+	}
+
 	public static void main(String[] args) {
 		UiUtil.printBreakLine();
 		greet();
 		UiUtil.printBreakLine();
-		runTaskProgram();
+		runTaskCliProgram();
 		UiUtil.printBreakLine();
 	}
 
 	private static void greet() {
-		String greeting = Constants.LOGO + "\n" + "Hello! I'm Yappy\n" + "What can I do for you?";
-		System.out.println(greeting);
+		System.out.println(getGreetings());
 	}
 
-	private static TaskList attemptToLoadTasksFromBackup() {
-		TaskList taskList;
-		try {
-			taskList = TaskList.usingBackup(Constants.TASKS_SAVE_FILE);
-			System.out.println("Successfully synced tasks from backup");
-		} catch (TaskListLoadBackupException e) {
-			System.out.println(e.getMessage());
-			taskList = new TaskList();
-		}
-		return taskList;
-
-	}
-
-	private static void runTaskProgram() {
+	private static void runTaskCliProgram() {
 		TaskList taskList = attemptToLoadTasksFromBackup();
-		UiUtil.printBreakLine();
 
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
 
 		while (true) {
-			String commandName = "";
-			String intermediateArgStr = "";
-			if (!input.isBlank()) {
-				String[] parsedInput = input.trim().split("\\s+", 2);
-				commandName = parsedInput[0];
-				if (parsedInput.length > 1) {
-					intermediateArgStr = parsedInput[1];
-				}
-			}
-			String argStr = intermediateArgStr;
+			ParsedInput parsedInput = parseInput(input);
+
 
 			UiUtil.printBreakLine();
 
-			Command.fromName(commandName).map(cmd -> {
-				try {
-					return cmd.execute(argStr, taskList);
-				} catch (YappyException e) {
-					return e.getMessage();
-				}
-			}).ifPresentOrElse(result -> {
-				System.out.println(result);
-				try {
-					taskList.save(Constants.TASKS_SAVE_FILE);
-				} catch (TaskListSaveBackupException e) {
-					System.out.println(e.getMessage());
-				}
-			}, () -> {
-				System.out.println("Unkown command. Supported commands are:");
-				for (Command validCommand : Command.values()) {
-					System.out.println(" - " + validCommand.getCommandInfo().name());
-				}
-			});
+			CommandResult result = Yappy.executeCommand(parsedInput, taskList);
+			taskList = result.taskList();
+			System.out.println(result.response());
 
-
-			if (commandName.equals(Command.EXIT.getCommandInfo().name())) {
+			if (parsedInput.commandName().equals(Command.EXIT.getCommandInfo().name())) {
 				break;
 			}
 			UiUtil.printBreakLine();
 			input = scanner.nextLine();
 		}
 		scanner.close();
+	}
+
+	private static TaskList attemptToLoadTasksFromBackup() {
+		TaskList taskList;
+		try {
+			taskList = TaskList.usingBackup(Constants.TASKS_SAVE_FILE);
+		} catch (TaskListLoadBackupException e) {
+			taskList = new TaskList();
+		}
+		return taskList;
+
+	}
+
+	private static ParsedInput parseInput(String input) {
+		String commandName = "";
+		String argStr = "";
+		if (!input.isBlank()) {
+			String[] tokens = input.trim().split("\\s+", 2);
+			commandName = tokens[0];
+			if (tokens.length > 1) {
+				argStr = tokens[1];
+			}
+		}
+		return new ParsedInput(commandName, argStr);
+	}
+
+	private static CommandResult executeCommand(ParsedInput parsedInput, TaskList taskList) {
+		return Command.fromName(parsedInput.commandName()).map(cmd -> {
+			try {
+				String response = cmd.execute(parsedInput.args(), taskList);
+				return new CommandResult(response, taskList);
+			} catch (YappyException e) {
+				return new CommandResult(e.getMessage(), taskList);
+			}
+		}).map(result -> {
+			try {
+				taskList.save(Constants.TASKS_SAVE_FILE);
+				return result;
+			} catch (TaskListSaveBackupException e) {
+				return new CommandResult(result.response() + "\n" + e.getMessage(),
+						result.taskList());
+			}
+		}).orElseGet(() -> {
+			StringBuilder sb = new StringBuilder("Unknown command. Supported commands are:\n");
+			for (Command validCommand : Command.values()) {
+				sb.append(" - ").append(validCommand.getCommandInfo().name()).append("\n");
+			}
+			return new CommandResult(sb.toString(), taskList);
+		});
 	}
 }
