01c3ec6f964d3f65bed084ac29b5161fdd11009b
TastyVeggy <alexius2111@gmail.com>
2025-09-14 15:51:24 +0800

    Add GUI program title

--- src/main/java/yappy/ui/gui/Main.java
+++ src/main/java/yappy/ui/gui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Yappy");
             fxmlLoader.<MainWindow>getController().setYappy(yappy); // inject the Yappy instance
             stage.show();
         } catch (IOException e) {
